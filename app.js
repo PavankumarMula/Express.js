fe0bced0cd45350bd64c895b2059be642aee7a94
PavankumarMula <pavanpappy7@gmail.com>
2023-05-29 23:43:52 +0530

    User Form

--- app.js
+++ app.js
@@ -1,121 +1,37 @@
-/* const http=require("http");
-const express=require("express");
-const bodyParser=require('body-parser');
-const app=express();
-const adminRoutes=require('./routes/adminRoutes');
-const shopRoutes=require('./routes/shopRoutes');
-const notFound=require('./routes/404notFound');
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(adminRoutes);
-app.use(shopRoutes);
-app.use(notFound);
-
-app.listen(4000, () => {
-  console.log('Server started on port 4000');
-});
-
-/* Routing with express.js
-app.get("/",(req,res)=>{
-    res.send("<h2>Home page</h2>");
-})
-
-app.get("/about",(req,res)=>{
-    res.send("<h2>About page</h2>");
-})
-
-app.get("/about-us",(req,res)=>{
-    res.redirect("/about");
-})
-
-app.use((req,res)=>{
-    res.send("404 not found");
-})
-
-app.use((req,res,next)=>{
-    console.log("in the first midlle ware",req.url);
-    next();
-})
-
-app.use("/",(req,res,next)=>{
-    console.log("Inside / req")
-    res.send("Home")
-    
-})
-
-app.use("/add",(req,res,next)=>{
-    console.log("Inside /add req")
-    res.send("Add")
-})*/
-
-// app.get("/add-product",(req,res)=>{
-//     res.send(`  <form method="POST" action="/product">
-//     <input type="text" name="productName" placeholder="Product Name" />
-//     <input type="text" name="productSize" placeholder="Product Size" />
-//     <button type="submit">Add Product</button>
-//   </form>`);
-
-// });
-
-// app.post('/product', (req, res) => {
-//     console.log(req.body);
-//     res.redirect('/product');
-//   });
-
-//   app.get('/product', (req, res) => {
-
-//     res.send('Product Page');
-//   });
-
-// app.get("/",(req,res)=>{
-//     console.log(req.body);
-//     res.send("Hello Im Home");
-// })
-
-// Start the server  */
-
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const app = express();
-const loginRouter = require("./GroupChat.js/LoginRouter");
+const Shop=require('./routes/shopRoutes');
+
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(loginRouter);
 
-const chatFilePath = path.join(__dirname, "GroupChat", "chat.txt");
+app.use(Shop);
+ 
+//showing user a form to fill up the data
+app.get('/contact',(req,res)=>{
+  const filePath=path.join(__dirname,"./","Data","Contact.html");
+  res.sendFile(filePath);
+})
 
-app.get("/", (req, res) => {
-  fs.readFile(chatFilePath, "utf8", (err, data) => {
-    if (err) {
-      res.send("Something went wrong");
-      return;
-    }
-    res.send(`${data}`);
-  });
-});
+//redirectiring the user to "/sucess url"
+app.post('/submit',(req,res)=>{
+  console.log(req.body);
+  res.redirect("/sucess");
+})
 
-app.post("/sentMessage", (req, res) => {
-  const username = req.body.username;
-  const message = req.body.typeMessage;
-  const data = `${username}:${message}`;
+//showing the user a sucess message
+app.get("/sucess",(req,res)=>{
+  res.send("Form Submitted Sucessfully");
+})
 
-  fs.appendFile(chatFilePath, data, (err) => {
-    if (err) console.log(err);
-  });
-
-  res.redirect("/");
-});
-
-// Create the chat file if it doesn't exist
-fs.access(chatFilePath, fs.constants.F_OK, (err) => {
-  if (err) {
-    fs.writeFile(chatFilePath, "", (err) => {
-      if (err) console.log(err);
-    });
-  }
-});
+//sending error message to user if he hits random url
+app.use((req,res)=>{
+  const filePath=path.join(__dirname, "./",'Data', '404Error.html');
+  res.status(404).sendFile(filePath);
+})
 
 // server portal
 app.listen(4000, () => {
